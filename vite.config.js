2dd26fa8e9779bde2f47ff6f67fc54ebe257df3f
kathimansilla <mansillakatherina@gmail.com>
2023-09-28 20:34:11 +0200

    add .github

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://kathimansilla.github.io/test-mecexis/',
+  base: 'https://kathimansilla.github.io/taskflow/',
 })
